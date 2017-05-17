7251dac0f90c6aa069b46bc65201d7aa4c184f17
emmaremy <eremy1@swarthmore.edu>
2017-05-17 16:30:39 -0400

    added debug?

--- volcano2.js
+++ volcano2.js
@@ -14,6 +14,7 @@ d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world
     if (error) throw error;
 
     point = [38.965, -77.067];
+    console.log(projection(point));
 
     svg.selectAll("path")
       .data(topojson.feature(world, world.objects.land).features)
