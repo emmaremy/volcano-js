a4be616679e398f269998065d79184236611f30c
emmaremy <eremy1@swarthmore.edu>
2017-05-17 15:42:29 -0400

    circle?

--- volcano2.js
+++ volcano2.js
@@ -8,10 +8,25 @@ var projection = d3.geoMercator()
     .rotate([-180,0]);
 
 var path = d3.geoPath()
-    .projection(projection)
+    .projection(projection);
+
+var data = [38.964, -77.067];
 
 d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
     if (error) throw error;
+
+    g.selectAll("circle")
+        .data(data)
+        .enter()
+      .append("circle")
+        .attr("cx", function(d) {
+            return projection([d.lon, d.lat])[0];
+        })
+        .attr("cy", function(d) {
+            return projection([d.lon, d.lat])[1];
+        })
+        .attr("r", 10)
+        .style("fill", "red")
 
     svg.append("g")
         .attr("class", "land")
