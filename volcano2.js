19fd5697254d2a2574872e71c330b5db14d2d442
emmaremy <eremy1@swarthmore.edu>
2017-05-17 17:53:23 -0400

    confused about the code i'm referencing

--- volcano2.js
+++ volcano2.js
@@ -16,7 +16,6 @@ d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world
 
     // must be long, lat
     point = [6.8, 50];
-    console.log(projection(point));
 
     svg.selectAll("path")
       .data(topojson.feature(world, world.objects.land).features)
@@ -25,20 +24,25 @@ d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world
         .attr("class", "land")
         .attr("d", path);
 
+d3.csv("volcano_comb.csv", function(error, data) {
+    if (error) throw error;
+
     svg.append("g")
         .selectAll("circle")
-        .data([point])
+        .data(data)
         .enter()
       .append("circle")
         .attr("cx", function(d) {
-            console.log(projection(d)[0]);
+            console.log(projection(d.longitude)[0]);
             return projection(d)[0];
         })
         .attr("cy", function(d) {
-            console.log(projection(d)[1]);
+            console.log(projection(d.latitude)[1]);
             return projection(d)[1];
         })
         .attr("r", 3)
         .style("fill", "red")
+
+});
 
 });
