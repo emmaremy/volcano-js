739a39a873f22b2b7696e3c7dab65ed8b4acbd54
emmaremy <eremy1@swarthmore.edu>
2017-05-17 15:24:29 -0400

    projection

--- volcano2.js
+++ volcano2.js
@@ -1,6 +1,16 @@
-var svg = d3.select("svg");
+var svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 var path = d3.geoPath();
+
+var projection = d3.geo.mercator()
+    .center([0, 5 ])
+    .scale(200)
+    .rotate([-180,0]);
+
+var path = d3.geoPath()
+    .projection(projection)
 
 d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
     if (error) throw error;
