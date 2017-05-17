be1c97945b6e722a0969065427ae18b246514d54
emmaremy <eremy1@swarthmore.edu>
2017-05-17 15:45:03 -0400

    ???

--- volcano2.js
+++ volcano2.js
@@ -12,11 +12,12 @@ var path = d3.geoPath()
 
 var data = [38.964, -77.067];
 
+var g = svg.append("g")
+
 d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
     if (error) throw error;
 
-    svg.append("g")
-        .attr("class", "land")
+    g.attr("class", "land")
       .selectAll("path")
       .data(topojson.feature(world, world.objects.land).features)
       .enter().append("path")
