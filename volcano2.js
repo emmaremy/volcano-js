01e0d2992e884c0e8a744e8bbfb22985a08b2fbc
emmaremy <eremy1@swarthmore.edu>
2017-05-17 17:15:41 -0400

    switched differently

--- volcano2.js
+++ volcano2.js
@@ -14,7 +14,7 @@ var path = d3.geoPath()
 d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
     if (error) throw error;
 
-    point = [-77.067, 33.9];
+    point = [38.965, -77.067];
     console.log(projection(point));
 
     svg.selectAll("path")
@@ -31,11 +31,11 @@ d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world
       .append("circle")
         .attr("cx", function(d) {
             console.log(projection(d)[0]);
-            return projection(d)[0];
+            return projection(d)[1];
         })
         .attr("cy", function(d) {
             console.log(projection(d)[1]);
-            return projection(d)[1];
+            return projection(d)[0];
         })
         .attr("r", 10)
         .style("fill", "red")
