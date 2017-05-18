5dceb7b1fdc4c1e2b3af5770fe7ac6ffa21c0f9e
emmaremy <eremy1@swarthmore.edu>
2017-05-18 09:34:56 -0400

    changed to long, lat

--- volcano2.js
+++ volcano2.js
@@ -33,12 +33,12 @@ d3.csv("volcano_comb.csv", function(error, data) {
         .enter()
       .append("circle")
         .attr("cx", function(d) {
-            console.log(projection(d)[0]);
-            return projection(d)[0];
+            console.log(projection([d.longitude, d.latitude])[0]);
+            return projection([d.longitude, d.latitude])[0];
         })
         .attr("cy", function(d) {
-            console.log(projection(d)[1]);
-            return projection(d)[1];
+            console.log(projection([d.longitude, d.latitude])[1]);
+            return projection([d.longitude, d.latitude])[1];
         })
         .attr("r", 3)
         .style("fill", "red")
